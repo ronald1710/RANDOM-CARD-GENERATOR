857f464d60f027f3024c98127b4e533e6357bb9f
ronald1710 <ronald.azofeifa@yahoo.com>
2022-09-01 01:57:00 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let simbolo = ["J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let random = Math.floor(Math.random() * (simbolo.length - 1));
+  document.querySelector("#numero").append(simbolo[random]);
+
+  let iconos = ["♦", "♥", "♠", "♣"];
+  let randomIconos = Math.floor(Math.random() * (iconos.length - 1));
+  document.querySelector("#simbolo").append(iconos[randomIconos]);
+  document.querySelector("#simbolo2").append(iconos[randomIconos]);
+  if (iconos[randomIconos] === "♦" || iconos[randomIconos] === "♥") {
+    document.querySelector("#simbolo").style.color = "red";
+    document.querySelector("#simbolo2").style.color = "red";
+  }
 };
